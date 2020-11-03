348cb06ae051f08f6d3bfa66f18db7a1dd9bb830
James Cleaver <k9barry@gmail.com>
2020-11-03 10:11:19 -0500

    try

--- startScript.php
+++ startScript.php
@@ -1,39 +1,49 @@
 <?php
 
-//Composer autoload
+/**
+ * Composer autoload
+ *
+ */
+
 require_once "./vendor/autoload.php";
 
+/**
+ * Load K9Barry namespace
+ *
+ */
+
 use K9Barry;
+
+/**
+ * Load Monolog and Initialize
+ *
+ */
+
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
+
+//$logger = new Logger('webhook_logger'); // Create the logger
+//$logger->pushHandler(new RotatingFileHandler("./data/Logs/webhook.log", Logger::DEBUG)); // Add sRotatingFileHandler
+//$logger->pushProcessor(new IntrospectionProcessor());
+//$logger->info('Webhook logger is now ready'); // You can now use your logger
 
 /**
  * startScript
  *
  */
 
- // Require config functions
+// Require config functions
 $configfile = "./config.php";
 if (file_exists($configfile)) {
     require_once $configfile;
 } else {
-    die ("Unable to locate config.php file");
+    die("Unable to locate config.php file");
 }
 
 ini_set('memory_limit', '-1');
 ini_set("max_execution_time", 0);
 set_time_limit(0);
-
-/**
- * Load Monolog and Initialize
- *
- */
-use Monolog\Handler\RotatingFileHandler;
-use Monolog\Logger;
-use Monolog\Processor\IntrospectionProcessor;
-
-$logger = new Logger('webhook_logger'); // Create the logger
-$logger->pushHandler(new RotatingFileHandler($strLogFolder . "/webhook.log", Logger::DEBUG)); // Now add some handlers
-$logger->pushProcessor(new IntrospectionProcessor());
-$logger->info('Webhook logger is now ready'); // You can now use your logger
 
 /**
  * Start monitoring
