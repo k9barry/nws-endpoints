0ea3104178512787f2496bfec5ebb8cf6d8b5383
James Cleaver <k9barry@gmail.com>
2020-11-03 09:34:29 -0500

    moved

---
+++ src/K9Barry/Webhook/openConnection.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace K9Barry\Webhook;
+
+class openConnection
+{
+    /**
+     * openConnection
+     *
+     * @param  mixed $db
+     * @return $db_conn  DB connection
+     */
+    public function openConnection($db)
+    {
+        $db_conn = new PDO("sqlite:$db");
+        $logger->info("Connection opened to database " . $db . "");
+        print_r($db_conn);
+        return $db_conn;
+    }
+}
---
+++ src/K9Barry/Webhook/sendActiveIncident.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace K9Barry\Webhook;
+
+class sendActiveIncident
+{
+    /**
+     * sendActiveIncident
+     *
+     * @param  mixed $db_conn
+     * @param  mixed $db_nwsCfsTableName
+     * @param  mixed $IncidentType
+     * @return true | false
+     */
+    public function sendActiveIncident($db_conn, $db_nwsCfsTableName, $IncidentType)
+    {
+        if (strpos($IncidentType, "|")) { // two incident types exist
+            $type = explode("|", $IncidentType);
+            $sql = "SELECT * FROM $db_nwsCfsTableName WHERE cfstype LIKE '$type[0]' OR cfstype LIKE '$type[1]'";
+        } else {
+            $sql = "SELECT * FROM $db_nwsCfsTableName WHERE cfstype LIKE '$IncidentType'";
+        }
+        $result = $db_conn->query($sql);
+        $n = 0;
+        foreach ($result as $row) {
+            if ($row['active'] == "Yes") {
+                $n++;
+            }
+        }
+        if ($n >= 1) {
+            $logger->info("Incident is whitelisted");
+            return true;
+        } else {
+            $logger->info("Incident is NOT whitelisted");
+            return false;
+        }
+    }
+}
