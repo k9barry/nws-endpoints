6f884d9aa31e28bd83b7314e9179e48b98a18f63
James <k9barry@gmail.com>
2021-02-28 15:55:31 -0500

    cleanup

--- src/K9Barry/Functions/fcn_sendSNPP.php
+++ src/K9Barry/Functions/fcn_sendSNPP.php
@@ -24,19 +24,10 @@ function fcn_sendSNPP($db_conn, $db_incident, $xml, $logger)
             throw new \Exception('Response: ' . $result);
             $logger->Error("Fsockopen error - " . $errstr($errno) . "");
         } else {
-            if (fgets($snpp) <> "220 Active911 SNPP ready, What is your emergency? ") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("Open connection to Active911 - " . fgets($snpp) . "");
             fwrite($snpp, "PAGE $snppPage\r\n");
-            if (fgets($snpp) <> "250 OK...") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("Execute PAGEr number to " . $snppPage . " - " . fgets($snpp) . "");
             fwrite($snpp, "DATA\r\n");
-            if (fgets($snpp) <> "354 Tell me exactly what is happening.") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("Set DATA protocol - " . fgets($snpp) . "");
             $out = $sep = '';
             foreach ($snppMessage[0] as $key => $value) {
@@ -45,20 +36,11 @@ function fcn_sendSNPP($db_conn, $db_incident, $xml, $logger)
             }
             fwrite($snpp, "$out\r\n");
             fwrite($snpp, ".\r\n");
-            if (fgets($snpp) <> "250 Message OK") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("\n" . $out . "");
             $logger->info("" . fgets($snpp) . "");
             fwrite($snpp, "SEND\r\n");
-            if (fgets($snpp) <> "250 Message Sent Successfully") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("Execute SEND - " . fgets($snpp) . "");
             fwrite($snpp, "QUIT\r\n");
-            if (fgets($snpp) <> "221 Good.") {
-                throw new \Exception('Response: ' . fgets($snpp));
-            }
             $logger->info("Execute QUIT - " . fgets($snpp) . "");
             fclose($snpp);
         }
