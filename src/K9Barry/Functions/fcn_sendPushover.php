1b5abbcf5ef023af32b052b3dc24656757a7c694
James <k9barry@gmail.com>
2021-02-28 14:04:01 -0500

    Fixed pushover errors

--- src/K9Barry/Functions/fcn_sendPushover.php
+++ src/K9Barry/Functions/fcn_sendPushover.php
@@ -57,7 +57,7 @@ function fcn_sendPushover($db_conn, $db_incident, $xml, $delta, $logger)
         }
         // Decode JSON data to PHP object
         $obj = json_decode($result);
-        if ($obj->status !== "1") {
+        if ($obj->status <> "1") {
             throw new \Exception('Response: ' . $result);
         }
     } catch (Exception $e) {
