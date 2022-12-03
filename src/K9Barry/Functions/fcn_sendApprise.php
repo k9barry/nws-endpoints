35afbe8a8b3e163dbb0a7e307c9ba4f8dda5acc2
James <k9barry@gmail.com>
2022-12-03 18:53:35 -0500

    Working in send - need to fix formatting

--- src/K9Barry/Functions/fcn_sendApprise.php
+++ src/K9Barry/Functions/fcn_sendApprise.php
@@ -26,13 +26,13 @@ function fcn_sendApprise($db_conn, $db_incident, $xml, $delta, $logger)
     extract($appriseMessage[0]);
     $urlEncFullAddress = urlencode($db_FullAddress);
     $mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center=$db_LatitudeY,$db_LongitudeX&zoom=16&size=400x400&maptype=hybrid&&markers=color:green|label:$urlEncFullAddress%7C$db_LatitudeY,$db_LongitudeX&key=$googleApiKey";
-    $logger->info("Open connection to Pushover using Google Url " . $mapUrl . "");
+    $logger->info("Open connection to Apprise using Google Url " . $mapUrl . "");
     curl_setopt_array($ch = curl_init(), array(
         CURLOPT_URL => "$appriseUrl/$appriseKey",
-        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_RETURNTRANSFER => false,
         CURLOPT_POSTFIELDS => array(
             "title" => "MCCD Call: $db_CallNumber $db_CallType ($delta)",
-            "message" => "
+            "body" => "
             C-Name: $db_CommonName
             Loc: $db_FullAddress
             Inc: $db_CallType
@@ -43,23 +43,36 @@ function fcn_sendApprise($db_conn, $db_incident, $xml, $delta, $logger)
             Unit: $db_UnitNumber
             Time: $db_CreateDateTime
             Narr: $db_Narrative_Text",
+            "sound" => "bike",
+            "html" => "1",
             "attachment" => curl_file_create("$mapUrl", "image/jpeg"),
         ),
     ));
+    //Encode the array into JSON.
+    //$jsonDataEncoded = json_encode($jsonData);
+
+    //Tell cURL that we want to send a POST request.
+    //curl_setopt($ch, CURLOPT_POST, 1);
+
+    //Attach our encoded JSON string to the POST fields.
+    //curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
+
+    //Set the content type to application/json
+    //curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
     try {
         $result = curl_exec($ch);
         if (curl_error($ch)) {
             throw new \Exception(curl_error($ch), curl_errno($ch));
         }
         // Decode JSON data to PHP object
-        $obj = json_decode($result);
-        if ($obj->status <> "1") {
-            throw new \Exception('Response: ' . $result);
-        }
+        //$obj = json_decode($result);
+        //if ($obj->status <> "1") {
+        //    throw new \Exception('Response: ' . $result);
+        //}
     } catch (Exception $e) {
         // exception is raised and it'll be handled here
         // $e->getMessage() contains the error message
-        $logger->Error("ERROR ". $e->getMessage() ."");
+        $logger->Error("ERROR " . $e->getMessage() . "");
     }
     curl_close($ch);
     $logger->info("Apprise message sent - " . $result . "");
