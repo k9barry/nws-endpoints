a57b952c12fb9ca1645737acda97508edc436ab6
K9 Barry <k9barry@gmail.com>
2023-08-28 09:49:29 -0400

    Update and rename fcn_recordReceived.php to fcn_13_recordReceived.php

--- src/K9Barry/Functions/fcn_13_recordReceived.php
+++ src/K9Barry/Functions/fcn_13_recordReceived.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * fcn_recordReceived
+ * fcn_13_recordReceived
  *
  * @param  mixed $db_conn
  * @param  mixed $db_incident
@@ -9,20 +9,20 @@
  * @param  mixed $logger
  * @return void
  */
-function fcn_recordReceived($db_conn, $db_incident, $strInFile, $logger)
+function fcn_13_recordReceived($db_conn, $db_incident, $strInFile, $logger)
 
 {
     $xml = simplexml_load_file($strInFile) or die("Error: Cannot create object");
     $logger->info("File " . $strInFile . " read into simpleXML");
     if ($xml->ClosedFlag == "true") { //record is closed
         $logger->info("ClosedFlag is true so remove record " . $xml->CallId . " from db");
-        fcn_deleteRecord($db_conn, $db_incident, $xml->CallId, $logger);
+        fcn_14_deleteRecord($db_conn, $db_incident, $xml->CallId, $logger);
         return;
-    } elseif (!fcn_callIdExist($db_conn, $db_incident, $xml->CallId, $logger)) { // record does not exist in db
+    } elseif (!fcn_15_callIdExist($db_conn, $db_incident, $xml->CallId, $logger)) { // record does not exist in db
         $logger->info("New record entered into DB set send = 1");
-        fcn_insertRecord($db_conn, $db_incident, $xml, $send = 1, $logger); // This is where a new record gets entered into db
+        fcn_16_insertRecord($db_conn, $db_incident, $xml, $send = 1, $logger); // This is where a new record gets entered into db
     } else {
         $logger->info("Record exists in DB - check for changes to requsite fields");
-        fcn_insertRecord($db_conn, $db_incident, $xml, $send = 0, $logger); // Record exists in DB-check for changes to in fields
+        fcn_16_insertRecord($db_conn, $db_incident, $xml, $send = 0, $logger); // Record exists in DB-check for changes to in fields
     }
 }
