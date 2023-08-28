90a83310baa4814718064c8452a45f0bae00a42f
K9 Barry <k9barry@gmail.com>
2023-08-28 10:02:57 -0400

    Update and rename fcn_closeConnection.php to fcn_17_closeConnection.php

--- src/K9Barry/Functions/fcn_17_closeConnection.php
+++ src/K9Barry/Functions/fcn_17_closeConnection.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * fcn_closeConnection
+ * fcn_17_closeConnection
  *
  * @param  mixed $db_conn
  * @param  mixed $logger
  * @return void
  */
-function fcn_closeConnection($db_conn, $logger)
+function fcn_17_closeConnection($db_conn, $logger)
 {
     $db_conn = null;
     $logger->info("Connection to database closed");
