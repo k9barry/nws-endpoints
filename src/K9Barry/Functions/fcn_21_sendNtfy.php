1df9b9aac5683a67f10f666040a12292103db603
K9 Barry <k9barry@gmail.com>
2024-01-11 13:51:21 -0500

    Update fcn_21_sendNtfy.php
    fix

--- src/K9Barry/Functions/fcn_21_sendNtfy.php
+++ src/K9Barry/Functions/fcn_21_sendNtfy.php
@@ -12,7 +12,7 @@
  * @param  mixed $resendAll
  * @return void
  */
-function fcn_21_sendNtfy($db_conn, $db_incident, $xml, $delta, $logger, $topics, $resendAll, $sendPushover)
+function fcn_21_sendNtfy($db_conn, $db_incident, $xml, $delta, $logger, $topics, $resendAll, $pushoverSend)
 {
     global $ntfyUrl, $ntfyToken, $ntfyUser, $googleApiKey;
     $CallId = $xml->CallId;
@@ -84,8 +84,8 @@ Narr: $db_Narrative_Text"
             $logger->info("========= Ntfy messages sent to topic " . $topic . " =========");
         } //foreach loop
     } // if !str_contains New Call
-    if ($sendPushover = true) {
-        fcn_21a_sendPushover();
+    if ($pushoverSend = true) {
+        fcn_21a_sendPushover($db_conn, $db_incident, $xml, $delta, $logger);
     }
 
     fcn_22_removeOldRecords($db_conn, $db_incident, $CallId, $logger);
